c1d7998d2b7fd512644c40b8ab0c0d5b8b53a7b1
laugmo <lauragarciamo@hotmail.com>
2016-10-25 23:12:24 -0300

    nameSearch

--- ProyectoFinal/public_html/apiMove/public/js/modules/home.js
+++ ProyectoFinal/public_html/apiMove/public/js/modules/home.js
@@ -4,41 +4,29 @@
 
 /* Home Module */
 
-angular.module('app.events', [])
+angular.module('app.home', [])
         .config(['$routeProvider', function config($routeProvider) {
                 $routeProvider.when('/home', {
-                    controller: 'EventController',
+                    controller: 'SearchEventController',
                     templateUrl: 'partials/_home.html'
                 });
-            }]);
+            }])
+        .controller('SearchEventController', ['$scope', function ($scope) {
+            $scope.searchEvent = function() {
+                if ($scope.searchTerm){
+                    dpd.events.get({ $or: 
+                        [{ name: $scope.searchTerm}, 
+                        { location: $scope.searchTerm}, 
+                        { sport: $scope.searchTerm }]
+                    }, 
+            function (result, err) {
+                if (err) {
+            // Alert if there's an error
+                 return alert(err.message || "Error al buscar eventos");
+                }
+            $scope.eventos = result;  
+                                   });
+                }
+            };
 
-angular.module('app.events')
-        .controller('EventController', ['$scope', function($scope) {
-                dpd.events.get(function (result, err) {
-                    if (err) {
-                        // Alert if there's an error
-                        return alert(err.message || "Error al buscar eventos");
-                    }
-                    $scope.eventos = result;
-                    
-                      function removeCompletedItems() {
-    // Get all completed todos
-    dpd.todos.get({completed: true}, function(todos) {
-      todos.forEach(function(t) {
-        // Delete it; "completed: true" is a failsafe
-        dpd.todos.del({id: t.id, completed: true}, function() {
-          // Now that's it's deleted from the server, remove the element, too
-          $('#' + t.id).remove();
-          
-          if (!$('#todos').children().length) {
-            $('#empty').show();
-          }
-        });
-      });
-    });
-    return false;
-}
-                    
-                    
-                });
-            }]);
+     }]);
