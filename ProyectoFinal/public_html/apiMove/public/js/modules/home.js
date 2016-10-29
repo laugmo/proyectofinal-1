9e15c5de8810b1734bb5e6023a645d5e40c93745
laugmo <lauragarciamo@hotmail.com>
2016-10-29 14:13:07 -0300

    Working Search Event

--- ProyectoFinal/public_html/apiMove/public/js/modules/home.js
+++ ProyectoFinal/public_html/apiMove/public/js/modules/home.js
@@ -2,23 +2,96 @@
 
 'use strict';
 
-/* Home Module */
+/* Home Module
+ * 
+ * var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('TodoController', function($scope, $http) {
+
+  $scope.todos = [];
+
+  // Get all todos
+  $http.get('/todos')
+    .success(function(todos) {
+      $scope.loaded = true;
+      $scope.todos = todos;
+    }).error(function(err) {
+      alert(err);
+    });
+
+  $scope.addTodo = function(title) {
+    $http.post('/todos', {
+      title: title
+    }).success(function(todo) {
+      $scope.newTodoTitle = '';
+      $scope.todos.push(todo);
+    }).error(function(err) {
+      // Alert if there's an error
+      return alert(err.message || "an error occurred");
+    });
+  };
+
+  $scope.changeCompleted = function(todo) {
+    // Update the todo
+    $http.put('/todos/' + todo.id, {
+      completed: todo.completed
+    }).error(function(err) {
+      return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
+    });
+  };
+
+  $scope.removeCompletedItems = function() {
+    $http.get('/todos', {
+      params: {
+        completed: true
+      }
+    }).success(function(todos) {
+      todos.forEach(function(t) { deleteTodo(t); });
+    });
+  };
+
+  function deleteTodo(todo) {
+    $http.delete('/todos/' + todo.id, {
+      params: {
+        completed: true
+      }
+    }).success(function() {
+      // Find the index of an object with a matching id
+      var index = $scope.todos.indexOf(
+          $scope.todos.filter(function(t) {
+            return t.id === todo.id;
+          })[0]);
+
+      if (index !== -1) {
+        $scope.todos.splice(index, 1);
+      }
+    }).error(function(err) {
+      alert(err.message || "an error occurred");
+    });
+  }
+
+});
+ *   */
+
+var moveApp = angular.module('app', []);
 
 angular.module('app.home', [])
         .config(['$routeProvider', function config($routeProvider) {
                 $routeProvider.when('/home', {
-                    controller: 'SearchEventController',
-                    templateUrl: 'partials/_home.html'
+                   templateUrl: 'partials/_home.html',
+                   controller: 'SearchEventController',
+                   controllerAs:'searcEventCtrl'
                 });
             }])
         .controller('SearchEventController', ['$scope', function ($scope) {
             $scope.searchEvent = function() {
                 if ($scope.searchTerm){
-                    dpd.events.get({ $or: 
+                   dpd.events.get({ $or: 
                         [{ name: $scope.searchTerm}, 
-                        { location: $scope.searchTerm}, 
-                        { sport: $scope.searchTerm }]
-                    }, 
+                        { area : $scope.searchTerm},
+                        { discipline: $scope.searchTerm }]
+                },
+                    
             function (result, err) {
                 if (err) {
             // Alert if there's an error
@@ -26,6 +99,7 @@ angular.module('app.home', [])
                 }
             $scope.eventos = result;  
                                    });
+                                   
                 }
             };
 
