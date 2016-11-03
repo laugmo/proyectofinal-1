7e2f5b53a6467d90f4e4fa656dae2b1195c333b1
letitechera <leticia.tech@gmail.com>
2016-11-03 00:52:03 -0300

    Login Form and authentification / still with some inconsistency

--- ProyectoFinal/public_html/apiMove/public/js/app.js
+++ ProyectoFinal/public_html/apiMove/public/js/app.js
@@ -1,9 +1,27 @@
 /* App Module */
 
-angular.module('app', ['app.events', 'app.home', 'app.user'])
+angular.module('app', ['app.home', 'app.user', 'app.events', 'app.login', 'app.register'])
         .config(function myAppConfig($routeProvider) {
             'use strict';
             $routeProvider.otherwise({redirectTo: '/home'});
         })
-  
-;
+        .controller('IndexController', ['$scope', function ($scope) {
+                var init = function () {
+                    dpd.users.me(function (user) {
+                        if (user) {
+                            alert("Welcome, " + user.username + "!");
+                        } else {
+                            location.href = "../partials/_login.html";
+                        }
+                    });
+                };
+                init();
+                
+                $scope.logout = function () {
+                    dpd.users.logout(function (res, err) {
+                        location.href = "../partials/_login.html";
+                    });
+                };
+            }]);
+
+        
