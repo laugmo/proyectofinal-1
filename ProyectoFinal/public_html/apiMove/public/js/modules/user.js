73811d59b90ca8ab53de6f7504efa22110b94df5
laugmo <lauragarciamo@hotmail.com>
2016-12-26 18:48:43 -0300

    sugerenciaUsuariosCorregida

--- ProyectoFinal/public_html/apiMove/public/js/modules/user.js
+++ ProyectoFinal/public_html/apiMove/public/js/modules/user.js
@@ -12,13 +12,13 @@ angular.module('app.user', [])
                     if (user) {
                         dpd.events.get(
                                 {$or: [
-                                {discipline: {$in: [user.sports]}},
-                                {area: {$regex: '^.*' + user.neighbourhood + '.*$',
-                                        $options: 'i'}}
-                            ]}, function (result, err) {
-                           if (err) {
+                                        {discipline: {$in: [user.sports]}},
+                                        {area: {$regex: '^.*' + user.neighbourhood + '.*$',
+                                                $options: 'i'}}
+                                    ]}, function (result, err) {
+                            if (err) {
                                 return alert(err.message || "Error al buscar eventos");
-                          }
+                            }
                             $scope.sugerencias = result;
                             $scope.$apply();
                         });
@@ -30,94 +30,36 @@ angular.module('app.user', [])
         .controller('SuggestUsers', ['$scope', function ($scope) {
                 dpd.users.me(function (user) {
                     if (user) {
-                        if (user.sports.length() > 0) {
-                            console.log(user.sports);
-                            var currentUser = user;
+                        if (user.sports.length > 0) {
+                            console.log(user.sports[0]);
+                            $scope.currentUser = user;
                             //console.log(currentUser.neighbourhood);
-                            var currentNeighbourhood = currentUser.neighbourhood;
-                            var currentId = currentUser.id;
-                            for (var i = 0; i < currentUser.sports.length(); i++) {
-                                var currentSport = currentUser.sports[i];
-                                dpd.users.get({
-                                    $and: [
-                                        {currentSport: {$in: [user.sports]}},
-                                        {neighbourhood: {$regex: '^.*' + currentNeighbourhood + '.*$',
-                                                $options: 'i'}},
-                                        {id: {$ne: currentId}}
-                                    ]}, function (result, err) {
-                                    if (err) {
-                                        return alert(err.message || "Error al buscar usuarios");
-                                    }
-                                    $scope.usuariosSugeridos = result;
-                                    $scope.$apply();
-                                });
-                                i++;
-                            }
+                            var currentNeighbourhood = user.neighbourhood;
+                            var currentUsername = user.username;
+                            var currentSports = user.sports;
+                            $scope.filterUsers(currentUsername,currentNeighbourhood,currentSports);
+
                         };
                     };
-                }, function (result, err) {
-                           if (err) {
+                });
+                $scope.filterUsers = function (currentUsername, currentNeighbourhood, currentSports) {
+                    for (var i = 0; i < currentSports.length; i++) {
+                        var sport = currentSports[i];
+                        dpd.users.get(
+                            {$and: [
+                                {username: {$ne: currentUsername}},
+                                {sports: {$in: [sport]}},
+                                {neighbourhood: {$eq:currentNeighbourhood}}
+                            ]},
+                        function (result, err) {
+                            if (err) {
                                 return alert(err.message || "Error al buscar usuarios");
-                          }
+                            }
                             $scope.usuariosSugeridos = result;
                             $scope.$apply();
                         });
+                    };
+                };
             }
         ]);
-//                        dpd.events.get(
-//                                {$or: [
-//                                {discipline: {$in: [user.sports]}},
-//                                {area: {$regex: '^.*' + user.neighbourhood + '.*$',
-//                                        $options: 'i'}}
-//                            ]}, function (result, err) {
-//                           if (err) {
-//                                return alert(err.message || "Error al buscar eventos");
-//                          }
-//                            $scope.sugerencias = result;
-//                            $scope.$apply();
-//                        });
-//                    }
-//                }
-//                );
 
-        //]);
-
-      /*.controller('SuggestUsers', ['$scope', function ($scope) {
-            $scope.searchEvent = function () {
-        if ($scope.searchTerm) {
-        dpd.users.me(function(user) {
-        if (user) {
-        dpd.events.get({$or:
-        [{name:{$in:[]}}]
-        }, function(result, err){});
-        }
-
-        // Alert if there's an error
-        return alert(error.message || "No se pudo obtener el usuario");
-        } else{
-        currentUser = result;
-                // Do something
-                });
-//                        dpd.events.get({$or:
-//                                    [{name: {$regex: '^.*' + $scope.searchTerm + '.*$',
-//                $options: 'i'
-//              }},
-//                                        {area: {$regex: '^.*' + $scope.searchTerm + '.*$',
-//                $options: 'i'
-//              }},
-//                                        {discipline: {$regex: '^.*' + $scope.searchTerm + '.*$',
-//                $options: 'i'
-//              }}]
-//                        },
-//                                function (result) {
-//                                   if (err) {
-//                                        // Alert if there's an error
-//                                        return alert(err.message || "Error al buscar eventos");
-//                                    }*/
-//                                    $scope.eventos = result;
-//                                });
-//
-//                    }
-//                };
-//
-//            }]);
